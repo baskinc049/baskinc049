44206f4cc4987383391d0fe0a958d6dcfc7f1627
baskinc049 <49683814+baskinc049@users.noreply.github.com>
2023-09-14 10:52:38 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -191,11 +191,11 @@ const IEMobile = navigator.userAgent.match(/IEMobile/i);
 //Smartphones and Additional Mobile Devices - Portrait & Landscape
 if (window.matchMedia('screen and (max-width: 320px)').matches || window.matchMedia('screen and (max-width: 480px)').matches || iOS || Android || IEMobile) {
     //Alert to view complex visualization on larger screen
-    confirm("Thank you for you interest in view my online portfolio! For the best viewing experience please view the website on either a larger screen (desktop or tablet) or in landscape orientation.")
+    confirm("Thank you for your interest in view my online portfolio! For the best viewing experience, please view the website on either a larger screen (desktop or tablet) and/or in landscape orientation.")
 }
 
 //iPads - Portrait
 if (window.matchMedia('screen and (max-width: 768px) and (orientation: landscape)').matches) {
     //Alert to view complex visualization on larger screen
-    confirm("Thank you for you interest in view my online portfolio! For the best viewing experience please view the website on either a larger screen (desktop or tablet) or in landscape orientation.")
+    confirm("Thank you for your interest in view my online portfolio! For the best viewing experience, please view the website on either a larger screen (desktop or tablet) and/or in landscape orientation.")
 }
